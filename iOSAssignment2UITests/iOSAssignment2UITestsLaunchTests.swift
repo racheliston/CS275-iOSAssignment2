297af4d6823a61a93afb41999a6acc0f3322662a
racheliston <rliston@uvm.edu>
2021-11-17 15:35:36 -0500

    Initial Commit

---
+++ iOSAssignment2UITests/iOSAssignment2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOSAssignment2UITestsLaunchTests.swift
+//  iOSAssignment2UITests
+//
+//  Created by Rachel Liston on 11/17/21.
+//
+
+import XCTest
+
+class iOSAssignment2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
