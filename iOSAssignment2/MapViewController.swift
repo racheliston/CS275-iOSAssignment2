eaeb04e52783f89132fcb37fbb92ca485780b687
racheliston <rliston@uvm.edu>
2021-11-17 16:49:11 -0500

    Added POI label and switch to map

--- iOSAssignment2/MapViewController.swift
+++ iOSAssignment2/MapViewController.swift
@@ -11,6 +11,9 @@ import MapKit
 class MapViewController: UIViewController {
     
     var mapView: MKMapView!
+    // Declare variables for POI label and switch
+    let poiLabel = UILabel()
+    let poiSwitch = UISwitch()
     
     override func loadView() {
         // Create a map view
@@ -31,6 +34,20 @@ class MapViewController: UIViewController {
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
         
+        // Assign text to POI label
+        poiLabel.text = "Points of Interest"
+        poiLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add POI label to view
+        view.addSubview(poiLabel)
+        
+        poiSwitch.translatesAutoresizingMaskIntoConstraints = false
+        poiSwitch.isOn = false
+        poiSwitch.addTarget(self, action: #selector(poiSelect(_:)), for: .valueChanged)
+                
+        // Add POI switch to view
+        view.addSubview(poiSwitch)
+        
         let topConstraint =
             segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
         
@@ -40,9 +57,24 @@ class MapViewController: UIViewController {
         let trailingConstraint =
             segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
         
+        // Constraints for POI label
+        let poiLabel1 = poiLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8)
+        let poiLabel2 = poiLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        
+        // Constraints for POI switch
+        let poiSwitch1 = poiSwitch.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8)
+        let poiSwitch2 = poiSwitch.leadingAnchor.constraint(equalTo: poiLabel.trailingAnchor, constant: 4)
+        let poiSwitch3 = poiSwitch.centerYAnchor.constraint(equalTo: poiLabel.centerYAnchor)
+        
+        // Activate all constraints
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
+        poiLabel1.isActive = true
+        poiLabel2.isActive = true
+        poiSwitch1.isActive = true
+        poiSwitch2.isActive = true
+        poiSwitch3.isActive = true
         
     }
     
@@ -56,6 +88,15 @@ class MapViewController: UIViewController {
             mapView.mapType = .satellite
         default:
             break
+        }
+    }
+    
+    // Function to control POI on map when switch is on or off
+    @IBAction func poiSelect(_ switchVal: UISwitch) {
+        if (switchVal.isOn) {
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter.includingAll
+        } else {
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
         }
     }
 
