71dcac45de2cd00867aef1bebf223539066a48ae
racheliston <rliston@uvm.edu>
2021-11-17 16:36:52 -0500

    Went through chapters 3 to 5 and implemented beginning of assignment

--- iOSAssignment2/ConversionViewController.swift
+++ iOSAssignment2/ConversionViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("ConversionViewController loaded its view.")
     }
-
-
+    
 }
 
